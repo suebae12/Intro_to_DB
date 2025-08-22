4be4072a1e0cfd560e0883670da12bac1f8dd1cd
Suebae <susanmarfo76@gmail.com>
2025-08-22 21:55:40 +0000

    Update bookstore database schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,27 +1,26 @@
---Create a database
-create database alx_book_store;
+CREATE DATABASE IF NOT EXISTS
+ alx_book_store;
 
---use the database
-use alx_book_store;
+-- Use the database
+USE alx_book_store;
 
---create a table for authors
-create table Authors(
-    author_id int primary key auto_increment,
-    author_name varchar(215) not null
+-- Create Authors table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
-
---create a table
-create table books(
-    book_id int Primary key auto_increment,
-    title VARCHAR(130) not null,
+-- Create Books table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
---Create a table for customers
+-- Create Customers table
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -29,7 +28,7 @@ CREATE TABLE Customers (
     address TEXT
 );
 
---Create a table for orders
+-- Create Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -37,16 +36,12 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-
---Create Order_Details table
+-- Create Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    price DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
---
